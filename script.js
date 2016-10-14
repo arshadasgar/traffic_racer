87898263d7d54f204a9221f8d62b8111af773a53
Arshad Muhammed <visitorsid@gmail.com>
2016-10-14 07:40:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,55 +17,24 @@ $(function () {
     var score = $('#score');
 
     //saving some initial setup
+    var container_left = parseInt(container.css('left'));
     var container_width = parseInt(container.width());
     var container_height = parseInt(container.height());
     var car_width = parseInt(car.width());
     var car_height = parseInt(car.height());
     
-    var car_1_top;
-    var car_2_top;
-    
-    var car_1_left;
-    var car_2_left;
-    var car_3_left;
-   
-    var car_1_top_initial = parseInt(car_1.css('top'));
-    var car_2_top_initial = parseInt(car_2.css('top'));
-    var car_3_top_initial = parseInt(car_3.css('top'));
-    
-    var car_1_left_initial = parseInt(car_1.css('left'));
-    var car_2_left_initial = parseInt(car_2.css('left'));
-    var car_3_left_initial = parseInt(car_3.css('left'));
-    
-    /*var line_1_top_initial = parseInt(line_1.css('top'));
-    var line_2_top_initial = parseInt(line_2.css('top'));
-    var line_3_top_initial = parseInt(line_3.css('top'));*/
-    
-    var line_1_top_initial = -300;
-    var line_2_top_initial = -300;
-    var line_3_top_initial = -300;
-
     //some other declarations
     var game_over = false;
-    
-    var count_1 = 1;
-    var count_2 = 1;
     
     var score_counter = 1;
     
     var speed = 2;
-
-    var speed_1 = 2;
-    var speed_2 = 2;
-    
     var line_speed = 5;
 
     var move_right = false;
     var move_left = false;
     var move_up = false;
     var move_down = false;
-
-    var who_won;
 
     /* ------------------------------GAME CODE STARTS HERE------------------------------------------- */
 
@@ -105,46 +74,50 @@ $(function () {
     });
 
     function left() {
-        if (parseInt(car.css('left')) > 0) {
+    	  if (game_over === false && parseInt(car.css('left')) > 0) {
             car.css('left', parseInt(car.css('left')) - 5);
             move_left = requestAnimationFrame(left);
         }
     }
 
     function right() {
-        if (parseInt(car.css('left')) < container_width - car_width) {
+        if (game_over === false && parseInt(car.css('left')) < container_width - car_width) {
             car.css('left', parseInt(car.css('left')) + 5);
             move_right = requestAnimationFrame(right);
         }
     }
     
     function up() {
-        if (parseInt(car.css('top')) > 0) {
+        if (game_over === false && parseInt(car.css('top')) > 0) {
             car.css('top', parseInt(car.css('top')) - 3);
             move_up = requestAnimationFrame(up);
         }
     }
     
     function down() {
-        if (parseInt(car.css('top')) < container_height - car_height) {
+        if (game_over === false && parseInt(car.css('top')) < container_height - car_height) {
             car.css('top', parseInt(car.css('top')) + 3);
             move_down = requestAnimationFrame(down);
         }
     }
 
-    /* Move the balls */
+    /* Move the cars and lines */
     anim_id = requestAnimationFrame(repeat);
 
     function repeat() {
         if (game_over === false) {
-            if (collision(car, car_1) || collision(car, car_2)) {
+            if (collision(car, car_1) || collision(car, car_2) || collision(car, car_3)) {
                 stop_the_game();
-                return;
             }
             
             score_counter++;
+            
             if(score_counter % 20 == 0){
                 score.text(parseInt(score.text()) + 1);
+            }
+            if(score_counter % 500 == 0){
+            	speed++;
+            	line_speed++;
             }
             
             car_down(car_1);   
@@ -160,26 +133,21 @@ $(function () {
     }
     
     function car_down(car){
-        var car_current_top = parseInt(eval(car).css('top'));
+        var car_current_top = parseInt(car.css('top'));
         if(car_current_top > container_height){
-            car_top = -200;  
-            var car_left = parseInt(Math.random() * 250);
-        }else{
-            car_top = car_current_top;
+            car_current_top = -200;
+            var car_left = parseInt(Math.random() * (container_width - car_width));
+            car.css('left', car_left);
         }
-        car.css('top', car_top + speed);
-        car.css('left', car_left);
+        car.css('top', car_current_top + speed);
     }
     
     function line_down(line){
-        var line_top;
         var line_current_top = parseInt(line.css('top'));
         if(line_current_top > container_height){
-            line_top = -300;    
-        }else{
-            line_top = line_current_top;
+            line_current_top = -300;    
         }
-        line.css('top', line_top + line_speed);
+        line.css('top', line_current_top + line_speed);
     }
 
     restart_btn.click(function () {
@@ -191,9 +159,12 @@ $(function () {
         cancelAnimationFrame(anim_id);
         cancelAnimationFrame(move_right);
         cancelAnimationFrame(move_left);
+        cancelAnimationFrame(move_up);
+        cancelAnimationFrame(move_down);
         restart_div.slideDown();
         restart_btn.focus();
     }
+    
     /* ------------------------------GAME CODE ENDS HERE------------------------------------------- */
 
 
@@ -218,78 +189,3 @@ $(function () {
 
 
 });
-
-/*function car_1_down(){
-        car_1_top_current = parseInt(car_1.css('top'));
-        if(car_1_top_current > container_height){
-            car_1_top = car_1_top_initial;    
-            car_1_left = parseInt(Math.random() * 300);
-            //if(count_1 % 3 == 0)
-            //  speed_1++;
-            //count_1++;
-        }else{
-            car_1_top = car_1_top_current;
-        }
-        car_1.css('top', car_1_top + speed);
-        car_1.css('left', car_1_left);
-    }
-    
-    function car_2_down(){
-        car_2_top_current = parseInt(car_2.css('top'));
-        if(car_2_top_current > container_height){
-            car_2_top = car_2_top_initial;  
-            car_2_left = parseInt(Math.random() * 250);
-            //if(count_2 % 3 == 0)
-             //   speed_2++;
-            //count_2++;
-        }else{
-            car_2_top = car_2_top_current;
-        }
-        car_2.css('top', car_2_top + speed);
-        car_2.css('left', car_2_left);
-    }
-    
-    function car_3_down(){
-        car_3_top_current = parseInt(car_3.css('top'));
-        if(car_3_top_current > container_height){
-            car_3_top = car_3_top_initial;  
-            car_3_left = parseInt(Math.random() * 250);
-            //if(count_2 % 3 == 0)
-            //    speed_2++;
-            //count_2++;
-        }else{
-            car_3_top = car_3_top_current;
-        }
-        car_3.css('top', car_3_top + speed);
-        car_3.css('left', car_3_left);
-    }*/
-    
-    /*function line_1_down(){
-        line_1_top_current = parseInt(line_1.css('top'));
-        if(line_1_top_current > container_height){
-            line_1_top = line_1_top_initial;    
-        }else{
-            line_1_top = line_1_top_current;
-        }
-        line_1.css('top', line_1_top + line_speed);
-    }
-    
-    function line_2_down(){
-        line_2_top_current = parseInt(line_2.css('top'));
-        if(line_2_top_current > container_height){
-            line_2_top = line_2_top_initial;    
-        }else{
-            line_2_top = line_2_top_current;
-        }
-        line_2.css('top', line_2_top + line_speed);
-    }
-    
-    function line_3_down(){
-        line_3_top_current = parseInt(line_3.css('top'));
-        if(line_3_top_current > container_height){
-            line_3_top = line_3_top_initial;    
-        }else{
-            line_3_top = line_3_top_current;
-        }
-        line_3.css('top', line_3_top + line_speed);
-    }*/
